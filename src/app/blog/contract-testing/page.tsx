2655b795698defb99ca7af25ea959504a5b50a34
Nick Wilder <nickwilder208@gmail.com>
2024-12-17 21:16:21 -0600

    Fix code markdown styles

--- src/app/blog/contract-testing/page.tsx
+++ src/app/blog/contract-testing/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { JSX } from "react";
 import ContractTesting from "./ContractTesting.mdx";
+import "../../../markdown-styles/github-dark.css";
 
 export default function Post(): JSX.Element {
   return (
