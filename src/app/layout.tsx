6af5421c8782955388f5f1ed85f3917633c1e84f
Nick Wilder <nick.wilder@esgglobal.com>
2025-02-22 12:15:38 -0600

    Google anayltics fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,12 @@ export const metadata: Metadata = {
     },
 };
 
+declare global {
+    interface Window {
+        dataLayer: any[];
+    }
+}
+
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -25,14 +31,15 @@ export default function RootLayout({
                     rel="stylesheet"
                 />
                 {/* Google Analytics */}
-                <script async src="https://www.googletagmanager.com/gtag/js?id=G-130R11B1E9"></script>
-                <script>
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-
-                  gtag('config', 'G-130R11B1E9');
-                </script>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-130R11B1E9" />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-130R11B1E9');
+                    `}
+                </Script>
 
                 <Script>{`console.log('%c'+\`${CONSOLE_ART}\`, '${consoleArtStyle}')`}</Script>
             </head>
