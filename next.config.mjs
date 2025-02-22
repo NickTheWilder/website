a9c872e187f670d3219edce51e889c29d406e57b
Nick Wilder <nick.wilder@esgglobal.com>
2025-02-22 11:57:22 -0600

    add prettier

--- next.config.mjs
+++ next.config.mjs
@@ -4,19 +4,19 @@ import rehypeHighlight from "rehype-highlight";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  productionBrowserSourceMaps: false,
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  images: {
-    domains: ["nickthewilder.com"],
-  },
+    productionBrowserSourceMaps: false,
+    pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+    images: {
+        domains: ["nickthewilder.com"],
+    },
 };
 
 const withMDX = createMDX({
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypeHighlight],
-    jsx: true,
-  },
+    options: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [rehypeHighlight],
+        jsx: true,
+    },
 });
 
 export default withMDX(nextConfig);
