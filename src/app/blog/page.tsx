1520d092ccb3be55b9e26f06c126eb67c4e911ac
Nick Wilder <nick.wilder@esgglobal.com>
2025-03-30 22:57:27 -0500

    i cannot spell quoutes to save my life - gun to my head i've misspelled this 04985304 times

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,15 +4,21 @@ import Link from "next/link";
 import { BlogPreview } from "../components/blogPreview";
 import { blogPosts, quotes } from "./data";
 import Header from "@/components/header";
+import { Quote, Tags } from "./types";
 
 export default function Blog(): JSX.Element {
-    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    const [quote, setQuote] = useState<Quote | null>(null);
+
+    useEffect(() => {
+        const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+        setQuote(randomQuote);
+    }, []);
 
     return (
         <div style={{ display: "block" }}>
             <Header />
             <div className={styles.intro}>
-                <h1 className={styles.introTitle}>Blog</h1>
+                <h1 className={styles.title}>Blog</h1>
                 <p>
                     Blog will contain notes from books, courses, and maybe occasional random thoughts. Most of the notes will come from my Obsidian vault served here as a backup using{" "}
                     <Link className={styles.link} href="https://mdxjs.com/">
@@ -22,14 +28,22 @@ export default function Blog(): JSX.Element {
                 </p>
                 <br />
                 <p>Notes will be added as I think they are relevant and polished enough to publish.</p>
-                <h2 className={styles.introTitle}>Recent Posts</h2>
+                <div className={styles.blogContainer}>
                 <div className={styles.blogGrid}>
                     {blogPosts.map((post) => (
                         <BlogPreview key={post.route} title={post.title} description={post.description} date={post.date} route={post.route} />
                     ))}
-                    <div className={styles.quoteContainer}>
-                        <div className={styles.quote}>{randomQuote.quote}</div>
-                        <div className={styles.author}>{randomQuote.author}</div>
+                    </div>
+                    <div className={styles.sidebar}>
+                        <div className={styles.quotes}>
+                            {quote !== null && (
+                                <>
+                                    <h2 className={styles.title}>Random Quote</h2>
+                                    <i className={styles.quote}>{quote?.quote}</i>
+                                    <div className={styles.author}> - {quote?.author}</div>
+                                </>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
