08ac34ae6053075eae35d06c1c4c00e2fed5ca97
Nick Wilder <nickwilder208@gmail.com>
2026-01-26 22:33:27 -0600

    Fix eslint (again)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,10 +6,16 @@ export default [
         ignores: ["eslint.config.mjs", "pages/_document.js"],
     },
     {
+        files: ["**/*.ts", "**/*.tsx"],
         rules: {
             "@typescript-eslint/no-unused-vars": "error",
             "@typescript-eslint/no-explicit-any": "warn",
             "@typescript-eslint/consistent-type-imports": "warn",
+        },
+    },
+    {
+        files: ["**/*.{js,jsx,ts,tsx}"],
+        rules: {
             "react/self-closing-comp": "warn",
             "react/hook-use-state": "warn",
         },
