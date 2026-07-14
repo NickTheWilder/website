84a91bdc29ebf25a1ab3fc98115f67d2d55cd32c
Nick Wilder <nickwilder208@gmail.com>
2026-07-14 13:35:43 -0500

    Upgrade ESLint to 10

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,29 @@
-import nextConfig from "eslint-config-next";
-import { defineConfig } from "eslint/config";
+import nextPlugin from "@next/eslint-plugin-next";
+import reactHooks from "eslint-plugin-react-hooks";
+import tsParser from "@typescript-eslint/parser";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-    ...nextConfig,
     {
-        ignores: ["eslint.config.mjs", "pages/_document.js"],
+        files: ["**/*.{js,jsx,ts,tsx}"],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+                ecmaVersion: "latest",
+                sourceType: "module",
+            },
+        },
+        plugins: {
+            "@next/next": nextPlugin,
+            "react-hooks": reactHooks,
+        },
+        rules: {
+            ...nextPlugin.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
+        },
     },
+    globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
